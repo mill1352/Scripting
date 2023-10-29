e4e1af66546ea7b013c6bf8983e201b886e45fcb
mill1352 <149300274+mill1352@users.noreply.github.com>
2023-10-29 13:23:58 +0300

    Add files via upload

---
+++ HelloWord.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWord : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		//команда выводид в консоль приветствия миру
+		print ("Hello, World!");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
